a9d88796d315ed6e7c14441486b962cae3dbb5f2
Степан Рудаков <19stepan94@mail.ru>
2024-06-30 00:16:18 +0500

    начал делать первую задачу 4.7

---
+++ Task01.js
@@ -0,0 +1,8 @@
+  'use strict'
+
+  const rain = [Math.random()];
+  const randomRain = rain * 100;
+
+const items = randomRain.filter(item => randomRain >= 0 && randomRain <= 100);
+  return items;
+console.log(items);
