2930190c63f6097fca39cbfede2a13516ee7ee30
Степан Рудаков <19stepan94@mail.ru>
2024-07-01 00:30:54 +0500

    сделал задание 2 в 4.7

--- game01.js
+++ game01.js
@@ -1,23 +1,33 @@
   'use strict'
+  
 
-  const randomNumber = Math.round(Math.random() * 100);
+   function foo () {
+    const randomNumber = Math.round(Math.random() * 100);
+    console.log(randomNumber);
 
-  const number = prompt('Введите число от 1 до 100');
-
-  console.log(randomNumber);
-
-
-  // while (number !== randomNumber && number !== null) {
-  //   if (number > randomNumber) {
-  //     console.log('Меньше');
-  //     prompt('Введите число ');
-  //   } else if (number < randomNumber) {
-  //     console.log('Больше');
-  //     prompt('Введите число от 1');
-  //   } else if (number === randomNumber) {
-  //     console.log('Верно');
-  //   } 
-  // } 
+  while (true) {
+    var number = prompt('Введите число от 1 до 100');
+    if (number === null) {
+      return 'Игра окончена';
+    }
+    else if (isNaN(number)) {
+      alert('Введи число!');
+    }
+    else if (number > randomNumber) {
+      alert('Меньше! ' + ' Введите новый вариант ');
+    } 
+    else if (number < randomNumber) {
+      alert('Больше! ' + ' Введите число ');
+    }
+    else {
+      alert('Правильно!');
+      console.log('Правильно!')
+      return 'Вы победили';
+    }
+  }
+   }
+   
+   console.log(foo());
 
   
 
