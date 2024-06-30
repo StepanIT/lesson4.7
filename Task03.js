dafa244dff66a07a490910ed5def75f1078ee6d2
Степан Рудаков <19stepan94@mail.ru>
2024-06-30 17:41:00 +0500

    сделал третью задачу 4.7

--- Task03.js
+++ Task03.js
@@ -1,1 +1,22 @@
   'use strict'
+
+  function generateRandomArray(length, n, m, line) {
+    const result = [];
+    const range = Math.abs(m - n);
+    const min = Math.min(n, m);
+    const max = Math.max(n, m);
+    let i = 0;
+  
+    while (i < length) {
+      i++;
+      const randomNum = Math.ceil(Math.random() * range) + min;
+      if (randomNum % 2 === 0 && line === 'even')  {
+        result.push(randomNum);
+      } else if (!(randomNum % 2 === 0) && line === 'odd') {
+        result.push(randomNum);
+      }
+    }
+    return result;
+  }
+
+  console.log(generateRandomArray(100, 0, 60, 'odd'));
