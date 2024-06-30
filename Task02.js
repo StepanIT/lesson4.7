05af074bf44c7e0eedb9d5f5a4b65e18c342afdd
Степан Рудаков <19stepan94@mail.ru>
2024-06-30 17:30:20 +0500

    сделал вторую задачу 4.7

--- Task02.js
+++ Task02.js
@@ -1,1 +1,20 @@
   'use strict'
+
+  function generateRandomArray(length, n, m) {
+    const result = [];
+    const range = Math.abs(m - n);
+    const min = Math.min(n, m);
+    const max = Math.max(n, m);
+    let i = 0;
+  
+    while (i < length) {
+      i++;
+      const randomNum = Math.ceil(Math.random() * range) + min;
+      result.push(randomNum);
+  
+    }
+    return result;
+  }
+
+  console.log(generateRandomArray(100, 50, 60));
+
