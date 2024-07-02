d9f840448b63ffd17923eecda28a565454b3d1d0
Степан <19stepan94@mail.ru>
2024-07-02 12:31:05 +0500

    поправил четвертую задачу

--- Task03.js
+++ Task03.js
@@ -1,22 +1,22 @@
   'use strict'
 
-  function generateRandomArray(length, n, m, line) {
-    const result = [];
-    const range = Math.abs(m - n) + 1;
-    const min = Math.min(n, m);
-    const max = Math.max(n, m);
-    let i = 0;
+  // function generateRandomArray(length, n, m, line) {
+  //   const result = [];
+  //   const range = Math.abs(m - n) + 1;
+  //   const min = Math.min(n, m);
+  //   const max = Math.max(n, m);
+  //   let i = 0;
   
-    while (result.length < length) {
-      const randomNum = Math.floor(Math.random() * range) + min;
-      if (randomNum % 2 === 0 && line === 'even')  {
-        result.push(randomNum);
-      } else if (!(randomNum % 2 === 0) && line === 'odd') {
-        result.push(randomNum);
-      }
-    }
-    return result;
-  }
+  //   while (result.length < length) {
+  //     const randomNum = Math.floor(Math.random() * range) + min;
+  //     if (randomNum % 2 === 0 && line === 'even')  {
+  //       result.push(randomNum);
+  //     } else if (!(randomNum % 2 === 0) && line === 'odd') {
+  //       result.push(randomNum);
+  //     }
+  //   }
+  //   return result;
+  // }
 
-  console.log(generateRandomArray(10, 10, 12, 'even'));
-  console.log(generateRandomArray(20, 10, 13, 'odd'));
+  // console.log(generateRandomArray(10, 10, 12, 'even'));
+  // console.log(generateRandomArray(20, 10, 13, 'odd'));
--- Task04.js
+++ Task04.js
@@ -1,7 +1,7 @@
   'use strict'
 
-// const getLeapYears = (n, m) => Array
-//   .from({ length: m - n + 1 }, (x, i) => n + i)
-//   .filter(x => ((x % 100) && !(x % 4)) || !(x % 400));
+const getLeapYears = (n, m) => Array
+  .from({ length: Math.abs(m - n) + 1 }, (x, i) => n + i)
+  .filter(x => ((x % 100) && !(x % 4)) || !(x % 400));
 
-// console.log(getLeapYears(2010, 2024));
+console.log(getLeapYears(1900, -1900));
