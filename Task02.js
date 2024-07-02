049dc1e6123bc1ad727e8d5edcbe39d1dffb7731
Степан <19stepan94@mail.ru>
2024-07-02 12:33:24 +0500

    поправил 4 задачу

--- Task02.js
+++ Task02.js
@@ -1,20 +1,20 @@
   'use strict'
 
-  // function generateRandomArray(length, n, m) {
-  //   const result = [];
-  //   const range = Math.abs(m - n) + 1;
-  //   const min = Math.min(n, m);
-  //   const max = Math.max(n, m);
-  //   let i = 0;
+  function generateRandomArray(length, n, m) {
+    const result = [];
+    const range = Math.abs(m - n) + 1;
+    const min = Math.min(n, m);
+    const max = Math.max(n, m);
+    let i = 0;
   
-  //   while (i < length) {
-  //     i++;
-  //     const randomNum = Math.floor(Math.random() * range) + min;
-  //     result.push(randomNum);
+    while (i < length) {
+      i++;
+      const randomNum = Math.floor(Math.random() * range) + min;
+      result.push(randomNum);
   
-  //   }
-  //   return result;
-  // }
+    }
+    return result;
+  }
 
-  // console.log(generateRandomArray(20, 50, 60));
+  console.log(generateRandomArray(20, 50, 60));
 
--- game01.js
+++ game01.js
@@ -1,34 +1,31 @@
   'use strict'
   
 
-  //  function foo () {
-  //   const randomNumber = Math.round(Math.random() * 100);
-  //   console.log(randomNumber);
+  function foo () {
+    const randomNumber = Math.round(Math.random() * 100);
+    console.log(randomNumber);
 
-  // while (true) {
-  //   var number = prompt('Введите число от 1 до 100');
-  //   if (number === null) {
-  //     return 'Игра окончена';
-  //   }
-  //   else if (isNaN(number)) {
-  //     alert('Введи число!');
-  //   }
-  //   else if (number > randomNumber) {
-  //     alert('Меньше! ' + ' Введите новый вариант ');
-  //   } 
-  //   else if (number < randomNumber) {
-  //     alert('Больше! ' + ' Введите число ');
-  //   }
-  //   else {
-  //     alert('Правильно!');
-  //     console.log('Правильно!')
-  //     return 'Вы победили';
-  //   }
-  // }
-  //  }
-   
-  //  console.log(foo());
+  while (true) {
+    var number = prompt('Введите число от 1 до 100');
+    if (number === null) {
+      return 'Игра окончена';
+    }
+    else if (isNaN(number)) {
+      alert('Введи число!');
+    }
+    else if (number > randomNumber) {
+      alert('Меньше! ' + ' Введите новый вариант ');
+    } 
+    else if (number < randomNumber) {
+      alert('Больше! ' + ' Введите число ');
+    }
+    else {
+      alert('Правильно!');
+      console.log('Правильно!')
+      return 'Вы победили';
+    }
+  }
+}
 
-  
+  console.log(foo());
 
- 
